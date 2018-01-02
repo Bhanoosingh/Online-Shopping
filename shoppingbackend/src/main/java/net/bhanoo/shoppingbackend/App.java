8438f3c6170c4d49cd7cd0ba8aa5bc626c9dfade
Bhanoo Pratap Singh <bhanoosinghpacex1@gmail.com>
2018-01-02 23:55:13 +0530

    My Project Setup Completed Successfully.

---
+++ shoppingbackend/src/main/java/net/bhanoo/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.bhanoo.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
